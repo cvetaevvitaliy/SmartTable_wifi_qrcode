89cd11a8e700ece9446360a794aa480c0b5ec202
zzrgenius <zouzirun@163.com>
2016-11-07 12:01:31 +0800

    revise gpio and devier of lcd

--- Bsp/bsp_leds.h
+++ Bsp/bsp_leds.h
@@ -2,16 +2,16 @@
 #define __BSP_LEDS_H
 
 
-#define LED1_PORT  			GPIOD
-#define LED1_PIN  			GPIO_Pin_2
+#define LED1_PORT  			GPIOC
+#define LED1_PIN  			GPIO_Pin_8
 
-#define LED2_PORT  			GPIOC
-#define LED2_PIN  			GPIO_Pin_6
+#define LED2_PORT  			GPIOB
+#define LED2_PIN  			GPIO_Pin_15
 
 #define LED3_PORT  			GPIOB
 #define LED3_PIN  			GPIO_Pin_13
 
-#define LED4_PORT  			GPIOA
+#define LED4_PORT  			GPIOB
 #define LED4_PIN  			GPIO_Pin_8
 
 #define LED5_PORT  			GPIOB
@@ -20,17 +20,17 @@
 #define LED6_PORT  			GPIOB
 #define LED6_PIN  			GPIO_Pin_14
 
-#define LED7_PORT  			GPIOB
-#define LED7_PIN  			GPIO_Pin_15
+#define LED7_PORT  			GPIOC
+#define LED7_PIN  			GPIO_Pin_6
 
 #define LED8_PORT  			GPIOC
-#define LED8_PIN  			GPIO_Pin_12
+#define LED8_PIN  			GPIO_Pin_9
 
 #define LED9_PORT  			GPIOB
-#define LED9_PIN  			GPIO_Pin_0
+#define LED9_PIN  			GPIO_Pin_7
 
 #define LED10_PORT  		GPIOB
-#define LED10_PIN  			GPIO_Pin_1
+#define LED10_PIN  			GPIO_Pin_6
 
 //#define LED9_Toggle			LED9_PORT->ODR ^= LED9_PIN
 void RedLedOn(void);
--- Users/main.c
+++ Users/main.c
@@ -1,5 +1,7 @@
 #include "stm32f10x.h"
-#include "oled.h"
+#include "GUI.h"
+#include "QDTFT_demo.h"
+#include "Lcd_Driver.h"
 #include "stdio.h"
 #include "QRCodeApp.h"
 #include "bsp.h"
@@ -30,7 +32,7 @@ int main(void)
 	sht_io_config();	
 	Wifi_Init();
 	
-	OLED_Init();
+	 
 	DISPLAY_RENCODE_TO_TFT("1234567890122456");
 	UsbOutConfig();
 	while(1)
@@ -57,23 +59,24 @@ void DISPLAY_RENCODE_TO_TFT(u8 *qrcode_data)
 //	LCD_Fill(0,40,240,320,WHITE);
 	sprintf((char*)qrencode_buff,"size:%d",m_nSymbleSize);//将LCD ID打印到lcd_id数组。
 //	LCD_ShowString(10,40,200,16,16,qrencode_buff);		//显示LCD ID	 
- 	OLED_ShowString(10,1,"E",12);
+  
 //	OLED_Fill(10,10,100,50,1);
-	OLED_Refresh_Gram();
-	//OLED_ShowString2(10,10,qrencode_buff,12);
+	Lcd_Clear(GRAY0);
+	 
 	if(m_nSymbleSize*2>128)	
 	{
 	//	LCD_ShowString(10,60,200,16,16,(u8 *)"The QR Code is too large!");//太大显示不下
-		OLED_ShowString(10,60,(u8 *)"The QR Code is too large!",12);
+		 
 		return;
 	}
 	for(i=0;i<10;i++)
 	{
 		if((m_nSymbleSize*i*2)>240)	break;
 	}
-	p=3;//(i-3)*1;//点大小
-	x=(240-m_nSymbleSize*p)/2 -50;
-	y=1;
+	p=(i-1)*2;//点大小
+	//p = 3;
+	x=(128-m_nSymbleSize*p)/2;
+	y=QRCODE_Y;
 	sprintf((char*)qrencode_buff,"piont:%d",p);//将LCD ID打印到lcd_id数组。
 	//LCD_ShowString(10,60,200,16,16,qrencode_buff);
 	//OLED_ShowString(10,60,qrencode_buff,12); 
@@ -84,12 +87,11 @@ void DISPLAY_RENCODE_TO_TFT(u8 *qrcode_data)
 			//USART1_SendData(m_byModuleData[j][i]);
 			if(m_byModuleData[i][j]==1)
 			{
-				OLED_Fill(x+p*i,y+p*j,x+p*(i+1)-1,y+p*(j+1)-1,1);
-			//	LCD_Fill(x+p*i,y+p*j,x+p*(i+1)-1,y+p*(j+1)-1,BLACK);
+				LCD_Fill(x+p*i,y+p*j,x+p*(i+1)-1,y+p*(j+1)-1,BLACK);
 			}
 
 		}
 			
 	}
-	OLED_Refresh_Gram();
+	 
 }
